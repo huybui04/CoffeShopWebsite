fa4cfd37d0fbcb0925f4badddd67d427396bfa2a
huybui04 <huyheo27112004@gmail.com>
2024-10-30 21:01:44 +0700

    Save local changes before merging dev branch

---
+++ btl_web_coffeeshop/Utilities/PaginationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace btl_web_coffeeshop.Utilities
+{
+    public static class PaginationExtensions
+    {
+        public static PagedList<T> ToPagedList<T>(this IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            var count = source.Count();
+            var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            return new PagedList<T>(items, count, pageIndex, pageSize);
+        }
+
+        public static async Task<PagedList<T>> ToPagedListAsync<T>(this IQueryable<T> source, int pageIndex, int pageSize)
+        {
+            if (pageIndex < 1) pageIndex = 1;
+            if (pageSize < 1) pageSize = 1;
+
+            try
+            {
+                var count = await source.CountAsync();
+                var items = await source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToListAsync();
+                return new PagedList<T>(items, count, pageIndex, pageSize);
+            }
+            catch (Exception ex)
+            {
+                // Log the exception or handle it accordingly
+                throw new Exception("An error occurred while fetching data: " + ex.Message);
+            }
+        }
+
+
+
+    }
+}
--- btl_web_coffeeshop/obj/Debug/net6.0/btl_web_coffeeshop.AssemblyInfo.cs
+++ btl_web_coffeeshop/obj/Debug/net6.0/btl_web_coffeeshop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("btl_web_coffeeshop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f62c41f46f00eaeebedaba383f94b2a2426c88")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04fa26a8437b175754630ec490ced6c00c303b53")]
 [assembly: System.Reflection.AssemblyProductAttribute("btl_web_coffeeshop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("btl_web_coffeeshop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
